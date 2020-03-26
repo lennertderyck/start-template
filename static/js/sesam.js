8b7d68001a0e2bd354644b22ddf70a9bd569588a
Lennert De Ryck <hello@lennertderyck.be>
2020-03-26 02:07:21 +0100

    Cleaned up some code
    Corona 🤷

--- static/js/sesam.js
+++ static/js/sesam.js
@@ -1,111 +1,87 @@
-(() => {
-    const sesam = {
-        initialize() {
-            console.log('\n' + `%c[service] sesam.js ${arguments.callee.name}() running! \n` + ' ', 'color: #00d400; font-weight: bold');
-            console.log(`%c[service] ${arguments.callee.name}()`, 'font-weight: bold');
-            this.cached();
-    
-            this.sesamTrigger.length !== 0 ? console.log(`\sesam triggers available`) : console.log(`\tno collapse triggers`);
-            this.sesamTrigger.forEach((trigger) => {
-                trigger.addEventListener("click", (() => {
-                    this.collapseDo(trigger.dataset.sesamTrigger);
-                }));
-    
-                if (trigger.classList.contains('sesam-hidden') == false && trigger.classList.contains('sesam-show') == false) {
-                    trigger.classList.add('sesam-hidden');
-                }
-                
-                if (trigger.classList.contains('sesam') == false) {
-                    trigger.classList.add('sesam');
-                }
-            });
-    
-            this.sesamTarget.forEach((target) => {
-                if (target.classList.contains('sesam-hidden') == false && target.classList.contains('sesam-show') == false) {
-                    target.classList.add('sesam-hidden');
-                }
-                
-                if (target.classList.contains('sesam') == false) {
-                    target.classList.add('sesam');
-                }
-            })
-        },
-    
-        cached() {
-            console.log(`%c[service] ${arguments.callee.name}()`, 'font-weight: bold');
-    
-            // Put cache elements here
-            this.sesamTrigger = document.querySelectorAll('[data-sesam-trigger]');
-            this.sesamTarget = document.querySelectorAll('[data-sesam-target]');
-            this.parent = '';
-            this.targetName = '';
-            this.trigger;
-        },
-    
-        collapseDo(target) {
-            console.log(`%c[service] ${arguments.callee.name}(${target})`, 'font-weight: bold');
-    
-            this.targetName = target;
-            target = document.querySelector(`[data-sesam-target='${target}']`);
-    
-            this.parent = target.dataset.sesamParent;
-            this.parent = document.querySelectorAll(`[data-sesam-group="${this.parent}"] .sesam`);
-            this.trigger = document.querySelector(`[data-sesam-trigger="${this.targetName}"]`);
-    
-            // COLLAPSE TRIGGER
-            if (this.trigger.classList.contains('sesam-show') == false) {
-                this.itemShow(this.trigger);
+export const sesam = {
+    initialize() {
+        console.log('\n' + `%c[service] sesam.js initialize() running! \n` + ' ', 'color: #00d400; font-weight: bold');
+        console.log(`%c[service] initialize()`, 'font-weight: bold');
+        this.cached();
 
-                // HIDE ALSO OTHER ELEMENTS
-                this.parent.forEach((item) => {
-                    if (item.dataset.sesamTrigger !== this.targetName) {
-                        this.itemHide(item);
-                    }
-                });
-            } else {
-                this.itemHide(this.trigger);
-            }
-    
-            // COLLAPSE TARGET
-            if (target.classList.contains('sesam-show') == false) {
-                this.itemShow(target)
-            } else {
-                this.itemHide(target)
+        this.sesamTrigger.length !== 0 ? console.log(`\sesam triggers available`) : console.log(`\tno collapse triggers`);
+        this.sesamTrigger.forEach((trigger) => {
+            trigger.addEventListener("click", (() => {this.collapseDo(trigger.dataset.sesamTrigger)}));
+            
+            if (trigger.classList.contains('sesam-hidden') == false && trigger.classList.contains('sesam-show') == false) trigger.classList.add('sesam-hidden');
+            if (trigger.classList.contains('sesam') == false) trigger.classList.add('sesam');
+        });
 
-                // HIDE ALSO OTHER ELEMENTS
-                this.parent.forEach((item) => {
-                    if (item.dataset.sesamTarget !== this.targetName) {
-                        this.itemHide(item);
-                    }
-                });
-            }
-        },
+        this.sesamTarget.forEach((target) => {
+            if (target.classList.contains('sesam-hidden') == false && target.classList.contains('sesam-show') == false) target.classList.add('sesam-hidden');
+            if (target.classList.contains('sesam') == false) target.classList.add('sesam');
+        })
+    },
 
-        itemHide(input) {
-            input.classList.remove('sesam-show');
-            input.classList.add('sesam-hidden');
-        },
+    cached() {
+        console.log(`%c[service] cached()`, 'font-weight: bold');
 
-        itemShow(input) {
-            input.classList.add('sesam-show');
-            input.classList.remove('sesam-hidden');
-        },
+        // Put cache elements here
+        this.sesamTrigger = document.querySelectorAll('[data-sesam-trigger]');
+        this.sesamTarget = document.querySelectorAll('[data-sesam-target]');
+        this.parent = '';
+        this.targetName = '';
+        this.trigger;
+    },
 
-        hideChildren(input) {
-            // input = the dataset for trigger or target
+    collapseDo(target) {
+        console.log(`%c[service] collapseDo(${target})`, 'font-weight: bold');
+
+        this.targetName = target;
+        target = document.querySelector(`[data-sesam-target='${target}']`);
+
+        this.parent = target.dataset.sesamParent;
+        this.parent = document.querySelectorAll(`[data-sesam-group="${this.parent}"] .sesam`);
+        this.trigger = document.querySelector(`[data-sesam-trigger="${this.targetName}"]`);
+
+        // COLLAPSE TRIGGER
+        if (this.trigger.classList.contains('sesam-show') == false) {
+            this.itemShow(this.trigger);
+
+            // HIDE ALSO OTHER ELEMENTS
             this.parent.forEach((item) => {
-                if (item.datatset.input !== this.targetName) {
-                    this.itemHide(item);
-                }
+                if (item.dataset.sesamTrigger !== this.targetName) this.itemHide(item);
             });
-        },
-
-        ifClassFalseAddClass(itemToApply = item, checkThisClass) {
-            if (itemToApply.classList.contains(checkThisClass) == false) {
-                itemToApply.classList.add(checkThisClass);
-            }
+        } else {
+            this.itemHide(this.trigger);
         }
-    }
 
-    sesam.initialize();
-})()
+        // COLLAPSE TARGET
+        if (target.classList.contains('sesam-show') == false) {
+            this.itemShow(target)
+        } else {
+            this.itemHide(target)
+
+            // HIDE ALSO OTHER ELEMENTS
+            this.parent.forEach((item) => {
+                if (item.dataset.sesamTarget !== this.targetName) this.itemHide(item);
+            });
+        }
+    },
+
+    itemHide(el) {
+        el.classList.remove('sesam-show');
+        el.classList.add('sesam-hidden');
+    },
+
+    itemShow(el) {
+        el.classList.add('sesam-show');
+        el.classList.remove('sesam-hidden');
+    },
+
+    hideChildren(input) {
+        // input = the dataset for trigger or target
+        this.parent.forEach((item) => {
+            if (item.datatset.input !== this.targetName) this.itemHide(item);
+        });
+    },
+
+    ifClassFalseAddClass(itemToApply = item, checkThisClass) {
+        if (itemToApply.classList.contains(checkThisClass) == false) itemToApply.classList.add(checkThisClass);
+    }
+}
